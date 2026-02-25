c5bec1b06a69b0839d233c85455427deff299b46
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-02-25 20:01:48 +0530

    first commit

---
+++ src/main/java/com/example/project_assignment/entity/Product.java
@@ -0,0 +1,37 @@
+package com.example.project_assignment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @Size(max=1000)
+    private String description;
+
+    @NotBlank
+    private String brand;
+
+    private String category;
+
+    @Positive
+    @DecimalMin(value = "0.0", inclusive = false)
+    private double price;
+    @CreationTimestamp
+    private LocalDateTime created_at;
+}
---
+++ src/test/java/com/example/project_assignment/ProjectAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
